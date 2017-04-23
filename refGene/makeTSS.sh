1b9590934a564b752b3f644cafbdcc76d8993a73
caleblareau <caleblareau@g.harvard.edu>
2017-04-23 15:50:44 -0400

    data dump

---
+++ refGene/makeTSS.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+awk '{if($4=="-"){v=$6}else{v=$5} print $3,v,v,$4}' OFS="\t" mm10.refGene.txt | awk 'length($1) < 6 {print $0}' | awk '$1 != "chrY" {print $0}' | sortBed -i stdin | uniq  > ../TSS/mm10.refGene.TSS.bed
+awk '{if($4=="-"){v=$6}else{v=$5} print $3,v,v,$4}' OFS="\t" mm9.refGene.txt | awk 'length($1) < 6 {print $0}' | awk '$1 != "chrY" {print $0}' | sortBed -i stdin | uniq  > ../TSS/mm9.refGene.TSS.bed
+awk '{if($4=="-"){v=$6}else{v=$5} print $3,v,v,$4}' OFS="\t" hg19.refGene.txt | awk 'length($1) < 6 {print $0}' | awk '$1 != "chrY" {print $0}' | sortBed -i stdin | uniq  > ../TSS/hg19.refGene.TSS.bed
+awk '{if($4=="-"){v=$6}else{v=$5} print $3,v,v,$4}' OFS="\t" hg38.refGene.txt | awk 'length($1) < 6 {print $0}' | awk '$1 != "chrY" {print $0}' | sortBed -i stdin | uniq  > ../TSS/hg38.refGene.TSS.bed
+
+
