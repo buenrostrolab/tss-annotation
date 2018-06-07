59c88640f4fac0c846f60c953619a31406729ce9
caleblareau <caleblareau@g.harvard.edu>
2018-06-07 16:09:35 -0400

    tss refresh

--- refGene/makeTSS.sh
+++ refGene/makeTSS.sh
@@ -8,3 +8,8 @@ awk '{if($4=="-"){v=$6}else{v=$5} print $3,v,v,$4}' OFS="\t" hg38.refGene.txt | 
 awk '{print substr($0,4)}' ../TSS/hg19.refGene.TSS.bed | awk '{print "human"$0}' > ../TSS/hg19_mm10_c.refGene.TSS.bed
 awk '{print substr($0,4)}' ../TSS/mm10.refGene.TSS.bed | awk '{print "mouse"$0}' >> ../TSS/hg19_mm10_c.refGene.TSS.bed
 
+awk '{print substr($0,4)}' ../TSS/hg19.refGene.TSS.bed | awk '{print "human"$0}' > ../TSS/hg19_mm10_c.refGene.TSS.bed
+awk '{print substr($0,4)}' ../TSS/mm10.refGene.TSS.bed | awk '{print "mouse"$0}' >> ../TSS/hg19_mm10_c.refGene.TSS.bed
+
+awk '{print substr($0,4)}' ../TSS/hg19.refGene.TSS.bed | awk '{print "human"$0}' > ../TSS/hg19-mm10.refGene.TSS.bed
+awk '{print substr($0,4)}' ../TSS/mm10.refGene.TSS.bed | awk '{print "mouse"$0}' >> ../TSS/hg19-mm10.refGene.TSS.bed
